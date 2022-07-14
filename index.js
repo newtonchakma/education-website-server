051261512c41fe26efa229dde14916c10d795ddd
newton chakma <newtonchakma304@gmail.com>
2022-07-15 01:07:45 +0600

    get all data in database

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zourtjs.mongodb.net/?retryWrites=true&w=majority`;
 console.log('hello',uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-/* client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-}); */
+
+  
+
 
 async function run(){
     try{
         await client.connect();
-        console.log('database connacted');
+        const serviceCollection = client.db("education_website").collection("services");
+
+        app.get('/service', async(req,res)=>{
+            const query ={};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
     }
     finally{
 
